d29cafae85ed4538b9d1ef89afd522caae64370a
Robert Whelan <whelan@gmail.com>
2024-05-14 16:28:47 -0500

    x

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
-import os, json
+import os, json, io
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import boto3
@@ -38,11 +39,11 @@ need the customer to be speaker 1
 - map campaigns & lead sources, to debt sources etc.
 """
 
-with open('normal-transcription.json') as f:
-    data = json.load(f)
-
-# contains each work spoken
-speech_data = data['results']['items']
+# with open('normal-transcription.json') as f:
+#     data = json.load(f)
+#
+# # contains each work spoken
+# speech_data = data['results']['items']
 
 #schema
 """
@@ -93,10 +94,33 @@ def download_mp3_from_html(url, save_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Example usage
-original_url = "https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE5f8b4ce0a74338da9a21e7bc0d4720b8"
-save_path = "downloads-new/mp3file.mp3"
-download_mp3_from_html(original_url, save_path)
+# # Example usage
+# original_url = "https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE5f8b4ce0a74338da9a21e7bc0d4720b8"
+# save_path = "downloads-new/mp3file.mp3"
+# download_mp3_from_html(original_url, save_path)
+
+call_urls = ['https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=REa8fae3124d2512357eeee072724f6ac0',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE7ddb9736596ff72af3a6e83aadb3f8be',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=REb6d960d0e8c20f62e95373569fee8f28',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE5f8b4ce0a74338da9a21e7bc0d4720b8',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=REe3343dc4545bf88c637c083d555401db',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=REdb2ef4ebfea7fe431da9deb4c3a9ddcb',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE5ffa36faa3134bb9b235b00a1fc3a6d4',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE0c54a5f09c9db90f54844cc6c317e570',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=REa9306adb927342f34f5d6f5ce52a9beb',
+             'https://admins.callerready.com/Recordings/Recording?AccountSid=AC3951a31053e8b50054f86fa930d61eb6&RecordingSid=RE140d7767d0b97cf299c70567499846ae']
+
+for call_url in call_urls:
+    save_path = f"downloads-1/{call_url.split('=')[-1]}.mp3"
+    download_mp3_from_html(call_url, save_path)
+
+
+# use boto3 s3 client to list all files in s3://synergy-sandbox-905418409497/test-transcribe-automation/
+s3=boto3.client('s3')
+response = s3.list_objects_v2(Bucket='synergy-sandbox-905418409497', Prefix='test-transcribe-automation/')
+keys = []
+for obj in response.get('Contents', []):
+    keys.append(obj['Key'])
 
 #read transcript into speaker turns
 def construct_transcript_turns(speech_data):
@@ -126,9 +150,25 @@ transcript_turns = construct_transcript_turns(speech_data)
 for turn in transcript_turns:
     print(turn)
 """
+def fetch_call_log():
+    s3 = boto3.resource('s3')
+    bucket_name = 'synergy-sandbox-905418409497'
+    object_key = 'sample-data-callerready/Call Log Advanced 4_1 4_15.csv'
+    obj = s3.Object(bucket_name, object_key)
+    data = obj.get()['Body'].read()
+    # put into a df
+    df = pd.read_csv(io.BytesIO(data), low_memory=False)
+    return df
 
+def get_call_urls(df):
+    # filter out calls that are > 20 seconds
+    # make list
+    call_urls = []
+    for call_url in df[df['Conference Time (seconds)'] > 20]['Recording']:
+        call_urls.append(call_url)
+    return call_urls
 def start_transcription_job(job_name, media_s3_uri, output_bucket, output_key):
-    transcribe_client=boto3.client('transcribe', region_name='us-east-1') #check bucket region
+    transcribe_client=boto3.client('transcribe', region_name='us-east-2') #check bucket region
     transcribe_client.start_transcription_job(
         TranscriptionJobName = job_name,
         Media = {
@@ -146,3 +186,16 @@ def start_transcription_job(job_name, media_s3_uri, output_bucket, output_key):
         },
         LanguageCode = 'en-US'
     )
+
+# start the transcription job with each key
+for key in keys:
+    #make the job name just the key without the prefix
+    job_name = key.split('/')[-1].split('.')[0]
+    print(job_name)
+    if len(job_name) > 0:
+        start_transcription_job(
+            job_name,
+            f's3://synergy-sandbox-905418409497/{key}',
+            'synergy-sandbox-905418409497',
+            f'transcription-output/{job_name}-transcription.json'
+        )
